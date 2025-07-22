ebd463526470401d902b915446f8ca5c9806bc7d
atif5 <atifaydinturanli@gmail.com>
2025-07-22 05:38:07 +0300

    debugging

--- rmtutoring-site/scripts/dashboard.js
+++ rmtutoring-site/scripts/dashboard.js
@@ -122,7 +122,7 @@ async function getVideoDate(video) {
     console.error("Error fetching video date:", data.error);
     return null;
   }
-  return new Date(data[0].startTime);
+  return formatDate(data[0].startTime);
 }
 
 function createFolder(video) {
